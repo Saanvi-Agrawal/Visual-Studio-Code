de24556fe61467118488d1509e77893693b64602
Saanvi-Agrawal <80035128+Saanvi-Agrawal@users.noreply.github.com>
2021-07-19 16:19:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+console.log("My first console project");
+var a=10;
+var b=5;
+var add=a+b;
+var sub=a-b;
+var mul=a*b;
+var div=a/b;
+console.log(add);
+console.log(sub);
+console.log(mul);
+console.log(div);
